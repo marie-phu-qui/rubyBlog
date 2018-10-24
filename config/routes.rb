7a22c5a7f73ef7ca5204ae3581077fce49680b64
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2018-10-24 13:52:04 +1300

    added bootstrap looks

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Route for the Root
-  root 'posts#index'
+  root 'posts#index', as: 'home'
   # Route for pages routes
-  get 'about'=> 'pages#about'
-  get 'contact'=> 'pages#contact'
+  get 'about'=> 'pages#about', as: 'about'
+  get 'contact'=> 'pages#contact', as: 'contact'
 
   # 
   resources :posts
